0b9732d5c661dc538fc75cecf4fe0bd292e68c4c
Enock Moseti <enock.g.moseti@gmail.com>
2022-04-30 14:31:02 +0300

    remove unused import

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Person } from 'src/app/person';
 import { PersonService } from 'src/app/services/person.service';
 
