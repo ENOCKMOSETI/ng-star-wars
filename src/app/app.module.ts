78984d5a65421e335af7218beb9fc1b2ca74c891
Enock Moseti <enock.g.moseti@gmail.com>
2022-05-01 04:23:47 +0300

    import forms module for [(ngModel)]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PersonComponent } from './components/person/person.component';
@@ -16,7 +17,8 @@ import { PersonComponent } from './components/person/person.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
